79495482ad63c18eb61bc3ae57be563521767b6a
Igor_Stetsenko <i.a.stetsenko@gmail.com>
2021-07-26 15:58:41 +0300

    add neuro files

---
+++ yolov5/detect.py
@@ -0,0 +1,86 @@
+import argparse
+import time
+import sys
+from pathlib import Path
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, \
+    strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+from yolov5.utils.datasets import *
+import yaml
+from yaml.loader import SafeLoader
+
+
+def detection_function(frame, source="yolov5/66521.jpg", stop_detection=True, view_img='store_true', save_txt=True,
+                       imgsz=640, save_img=False):
+    """The main detection function"""
+    box = []
+    weights = "yolov5/yolov5s.pt"
+    # Directories
+    save_dir = Path(increment_path(Path("runs/detect") / 'exp', exist_ok='store_true'))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize
+    set_logging()
+    device = select_device('')
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, device)  # load FP32 model\
+    imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
+    if half:
+        model.half()  # to FP16
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+
+    img0 = frame
+    img = letterbox(img0, new_shape=800)[0]
+
+    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+    img = np.ascontiguousarray(img)
+    img = torch.from_numpy(img).to(device)
+    img = img.half() if half else img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    # print(img.ndimension(), "++++")
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+        pred = model(img, augment=False)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, 0.4, 0.5, classes=None)
+        # t2 = time_synchronized()
+
+    # Process detections
+    for i, det in enumerate(pred):  # detections per image
+        gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+
+
+            with open('yolov5/models/coco.yaml') as f:
+                # читаем документ YAML
+                data_classes = yaml.load(f, Loader=SafeLoader)["names"]
+
+            for *xyxy, conf, cls in reversed(det):
+                x = xyxy
+                box_class = data_classes[int(cls)]
+                points = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+                #print(box_class)
+                if box_class == "truck" or "car":   #Add truck or car only
+                    box.append(points)
+                    #
+
+    return box
