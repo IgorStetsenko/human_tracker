a7bdaebc13e82e9b5523a888a0ec9ddcd0ee8080
IgorStetsenko <i.a.stetsenko@gmail.com>
2025-02-04 11:33:26 +0300

    add feature_1

--- main_tracker.py
+++ main_tracker.py
@@ -17,6 +17,10 @@ def test2():
 def test3():
     return none
 
+def feature_1():
+    return none
+
+
 # the below function was taken from
 # https://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
 def non_max_suppression(boxes, overlapThresh):
